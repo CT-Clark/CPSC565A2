8c76a3c9c11eed8eeb7c9c121d3f1805743e6464
Cody Clark <cody.t.clark@gmail.com>
2021-02-24 19:05:03 -0700

    Add files via upload

---
+++ SnitchManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnitchManager : MonoBehaviour
+{
+    private int m_RandomNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
