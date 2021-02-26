4ef592216420464b6ed61bb2a53c15e5b581998c
Cody Clark <cody.t.clark@gmail.com>
2021-02-26 04:45:44 -0700

    No longer spawn spawn locations

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -7,115 +7,63 @@ namespace Complete
 {
     public class GameManager : MonoBehaviour
     {
-        public int m_PointsToWin = 100;             // The number of points a team needs to win the game.
-        public float m_StartDelay = 3f;             // The delay between the start of RoundStarting and RoundPlaying phases.
-        public float m_EndDelay = 3f;               // The delay between the end of RoundPlaying and RoundEnding phases.
-        //public CameraControl m_CameraControl;       // Reference to the CameraControl script for control during different phases.
-        public Text m_MessageText;                  // Reference to the overlay Text to display
-        public GameObject m_Player;                 // Reference to the players.
-        public PlayerManager[] m_Players;           // A collection of managers for enabling and disabling different aspects of the game.
-        public TeamManager[] m_Teams;
-
-        private int m_RoundNumber;                  // Which round the game is currently on.
-        private WaitForSeconds m_StartWait;         // Used to have a delay whilst the round starts.
-        private WaitForSeconds m_EndWait;           // Used to have a delay whilst the round or game ends.
-        private TeamManager m_RoundWinner;          // Reference to the winner of the current round.  Used to make an announcement of who won.
-        private TeamManager m_GameWinner;           // Reference to the winner of the game.  Used to make an announcement of who won.
+        public int pointsToWin = 100;             // The number of points a team needs to win the game.
+        public Text messageText;                  // Reference to the overlay Text to display
+        public GameObject player;                 // Reference to the player object.
+        public GameObject snitch;                 // Reference to the snitch object
+        public PlayerManager[] players;           
+        public TeamManager[] teams;
+        public SnitchManager[] snitches;
+        private TeamManager roundWinner;          
+        private TeamManager gameWinner;    
+        
+        void Awake()
+        {
+            SpawnSnitch();
+        }
 
 
         private void Start()
         {
-            // Create the delays so they only have to be made once.
-            m_StartWait = new WaitForSeconds(m_StartDelay);
-            m_EndWait = new WaitForSeconds(m_EndDelay);
-
-            SpawnAllTeams();
-
-            // Once the Players have been created and the camera is using them as targets, start the game.
-            StartCoroutine(GameLoop());
+            
+            
         }
 
-
-        private void SpawnAllTeams()
+        // Updates tied to framerate
+        void Update()
         {
-            // For all the Teams...
-            for (int i = 0; i < m_Teams.Length; i++)
+            if (teams[0].points < 100 || teams[1].points < 100)
             {
-                m_Teams[i].Setup();
-            }
-        }
-
-
-        // This is called from start and will run each phase of the game one after another.
-        private IEnumerator GameLoop()
-        {
-            // Start off by running the 'RoundStarting' coroutine but don't return until it's finished.
-            yield return StartCoroutine(RoundStarting());
-
-            // Once the 'RoundStarting' coroutine is finished, run the 'RoundPlaying' coroutine but don't return until it's finished.
-            yield return StartCoroutine(RoundPlaying());
-
-            // Once execution has returned here, run the 'RoundEnding' coroutine, again don't return until it's finished.
-            yield return StartCoroutine(RoundEnding());
-
-            // This code is not run until 'RoundEnding' has finished.  At which point, check if a game winner has been found.
-            if (m_GameWinner != null)
-            {
-                // If there is a game winner, restart the level.
-                //SceneManager.LoadScene(0);
+                messageText.text = PlayingMessage();
             }
             else
             {
-                // If there isn't a winner yet, restart this coroutine so the loop continues.
-                // Note that this coroutine doesn't yield.  This means that the current version of the GameLoop will end.
-                StartCoroutine(GameLoop());
+                messageText.text = "";
             }
         }
 
-
-        private IEnumerator RoundStarting()
+        // Spawns the golden snitch
+        public void SpawnSnitch()
         {
-            // As soon as the round starts reset the Players and make sure they can't move.
-            ResetAllPlayers();
-
-            // Wait for the specified length of time until yielding control back to the game loop.
-            yield return m_StartWait;
+            float xcor, ycor, zcor;
+            Vector3 pos;
+            xcor = Random.Range(-100, 100);
+            ycor = Random.Range(1, 100);
+            zcor = Random.Range(-100, 100);
+            pos = new Vector3(xcor, ycor, zcor);
+            snitches[0].instance = Instantiate(snitch, pos, snitch.transform.rotation) as GameObject;
+            snitches[0].Setup();
         }
 
 
-        // Handles the period of time after the round starts and before the round ends
-        private IEnumerator RoundPlaying()
+        public string PlayingMessage()
         {
-            // Clear the text from the screen.
-            m_MessageText.text = string.Empty;
-            return null;
-        }
-
-        // What happens when the round ends
-        private IEnumerator RoundEnding()
-        {
-            // Clear the winner from the previous round.
-            m_RoundWinner = null;
-
-            // See if there is a winner now the round is over.
-            m_RoundWinner = GetRoundWinner();
-
-            // Now the winner's score has been incremented, see if someone has one the game.
-            m_GameWinner = GetGameWinner();
-
-            // Get a message based on the scores and whether or not there is a game winner and display it.
-            string message = EndMessage();
-            m_MessageText.text = message;
-
-            // Wait for the specified length of time until yielding control back to the game loop.
-            yield return m_EndWait;
-        }
-
-
-        private TeamManager GetRoundWinner()
-        {
-            //continue;
-            return null;
+            string msg = "";
+            for (int i = 0; i < teams.Length; i++)
+            {
+                msg += "<color=#" + ColorUtility.ToHtmlStringRGB(teams[i].teamColor) + ">" + teams[i].teamText + ": </color>" + teams[i].points + "\t\t"; 
+            }
+            return msg;
         }
 
 
@@ -123,54 +71,15 @@ namespace Complete
         private TeamManager GetGameWinner()
         {
             // Go through all the Teams...
-            for (int i = 0; i < m_Teams.Length; i++)
+            for (int i = 0; i < teams.Length; i++)
             {
                 // ... and if one of them has enough points to win the game, return it.
-                if (m_Teams[i].m_Points == m_PointsToWin)
-                    return m_Teams[i];
+                if (teams[i].points == pointsToWin)
+                    return teams[i];
             }
 
             // If no Teams have enough rounds to win, return null.
             return null;
-        }
-
-
-        // Returns a string message to display at the end of each round.
-        private string EndMessage()
-        {
-            // By default when a round ends there are no winners so the default end message is a draw.
-            string message = "DRAW!";
-
-            // If there is a winner then change the message to reflect that.
-            if (m_RoundWinner != null)
-                message = m_RoundWinner.m_TeamText + " WINS THE ROUND!";
-
-            // Add some line breaks after the initial message.
-            message += "\n\n\n\n";
-
-            /*
-            // Go through all the Players and add each of their scores to the message.
-            for (int i = 0; i < m_Players.Length; i++)
-            {
-                message += m_Players[i].m_ColoredPlayerText + ": " + m_Players[i].m_Wins + " WINS\n";
-            }
-            */
-
-            // If there is a game winner, change the entire message to reflect that.
-            if (m_GameWinner != null)
-                message = m_GameWinner.m_TeamText + " WINS THE GAME!";
-
-            return message;
-        }
-
-
-        // This function is used to turn all the Players back on and reset their positions and properties.
-        private void ResetAllPlayers()
-        {
-            for (int i = 0; i < m_Players.Length; i++)
-            {
-                m_Players[i].Reset();
-            }
         }
     }
 }
